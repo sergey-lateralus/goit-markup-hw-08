6fa71349402bc0154af54c652632c9f5fe053800
sergey-lateralus <sergey.nezhyn@gmail.com>
2021-06-28 02:35:28 +0300

    Добавляет мобильное меню

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,13 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
+  const mobileBtnClose = document.querySelector("[data-menu-close]");
 
   menuBtnRef.addEventListener("click", () => {
-    const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+    mobileMenuRef.classList.toggle("is-open");
+  });
 
-    menuBtnRef.classList.toggle("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
-
+  mobileBtnClose.addEventListener("click", () => {
     mobileMenuRef.classList.toggle("is-open");
   });
 })();
